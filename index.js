367ddb03b16b0a41cb11aba3ba4d97b6426ac112
Dionei J. Delangolo <dionei.jd@gmail.com>
2020-12-27 14:47:52 -0300

    fatored index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const puppeteer = require('puppeteer')
-const robot = require('./robots/LogWorkPoster.js')
 
 const robots = {
     logon: require('./robots/Logon.js'),
@@ -12,8 +11,6 @@ async function start() {
 
     console.log('>>>> START - Starting') 
 
-
-
     let browser
     await OpenBrowser(true)    
 
@@ -22,18 +19,8 @@ async function start() {
     await robots.logWorkPoster(browser)
 
     await browser.close()
-
-
     
     console.log('>>>> START - Finished')
-
-
-
-
-
-
-
-
 
 
     async function OpenBrowser(show){
@@ -46,103 +33,6 @@ async function start() {
 
         browser = await puppeteer.launch(option)
     }
-
-
-
-
-
-
-    
-    // await jiraLogon(cred)
-
-
-
-    // const wrkLog = {
-    //     id: '179820',
-    //     time: '1m',
-    //     date: '15/Dec/20 08:00 PM',
-    //     comment: 'test comment'
-    // }
-
-    // await postWorkLog(wrkLog)
-    
-    // console.log('>>>> RUN - Finished')
-
-
-
-
-
-    
-    
-    // console.log('>>>>>> log2 - INICIO')
-    // //const issueId = await page.$eval('#key-val', el => el.rel)
-    // //await page2.goto(`https://jira.weg.net/secure/CreateWorklog!default.jspa?id=${issueId}`)
-    // const page2 = await browser.newPage()
-    // await page2.goto('https://jira.weg.net/secure/CreateWorklog!default.jspa?id=179820')
-    
-    // await page2.evaluate(() => {
-    //     console.log('>>>>>> log2.1')    
-    //     document.querySelector('#log-work-time-logged').value = '1m'
-    //     document.querySelector('#log-work-date-logged-date-picker').value = '15/Dec/20 08:00 PM'
-    //     document.querySelector('#comment').value = 'Test comment'
-
-    //     document.querySelector('#log-work-submit').click()
-    // })
-
-    
-    // console.log('>>>>>> log2.2')   
-
-    // await page2.waitForNavigation()
-
-    // page2.close()
-
-    // console.log('>>>>>> log2 - FIM')    
-
 }
-
-
-
-
-
-// async function jiraLogon (credentials) {
-
-//     console.log('>>>> Jira Login - Starting')
-
-//     browser = await puppeteer.launch({ headless: false})
-
-//     const page = await browser.newPage()
-//     await page.goto('https://jira.weg.net')
-
-//     await page.type('#login-form-username', credentials.user)
-//     await page.type('#login-form-password', credentials.password)
-//     await page.click('#login, #login-form-submit')
-    
-//     await page.waitForNavigation()
-        
-//     console.log('>>>> Jira Login - Finished')
-
-// }
-
-
-// async function postWorkLog (worklog) {
-//     console.log('>>>> Post Work Log - Starting')
-
-//     const page = await browser.newPage()
-//     await page.goto('https://jira.weg.net/secure/CreateWorklog!default.jspa?id=179820')
-    
-//     await page.evaluate(() => {
-//         document.querySelector('#log-work-time-logged').value = '1m'
-//         document.querySelector('#log-work-date-logged-date-picker').value = '15/Dec/20 08:00 PM'
-//         document.querySelector('#comment').value = 'Test comment'
-
-//         document.querySelector('#log-work-submit').click()
-//     })
-
-//     await page.waitForNavigation()
-//     page.close()
-
-//     console.log('>>>> Post Work Log - Finished')
-// }
-
 
 start()
