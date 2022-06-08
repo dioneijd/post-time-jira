948bb48409b549500a460de293c8754c997c20ab
Dionei J. Delangolo <dionei.jd@gmail.com>
2022-06-08 08:09:38 -0300

    Added total of days

--- robots/LogWorkOrganizer.js
+++ robots/LogWorkOrganizer.js
@@ -39,14 +39,16 @@ async function robot(browser) {
         console.log('>>>> LOG_WORK_ORGANIZER - DefineWorkLogDate - Starting')
 
         const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        let resp, day, month, year
+        let resp, day, month, year, totalDays
 
         do {
             day = readline.question('Informar o dia (DD): ', { limit: input => input>=1 && input<=31, limitMessage: 'Must be a number between 1 and 31'})
             month = months[readline.question('Informar o dia (MM): ', { limit: input => input>=1 && input<=12, limitMessage: 'Must be a number between 1 and 12'}) - 1]
             year = readline.question('Informar o dia (YYYY): ', { limit: input => input>=2020 && input<=2100, limitMessage: 'Must be a number between 2020 and 2030'})
-        
-            resp = readline.question(`Do you confirm the date ${day}-${month}-${year} [Y/n]: `, {limit: input => input.toUpperCase() == 'Y' || input.toUpperCase() == 'N'}).toUpperCase()
+            
+            totalDays = Math.floor((new Date().getTime() - new Date(`${month}/${day}/${year}`).getTime()) / (1000 * 3600 * 24))
+
+            resp = readline.question(`Do you confirm the date ${day}-${month}-${year} (${totalDays} days ago) [Y/n]: `, {limit: input => input.toUpperCase() == 'Y' || input.toUpperCase() == 'N'}).toUpperCase()
         
             if (resp != 'Y') console.log('ok... Vamos tentar novamente:')
 
